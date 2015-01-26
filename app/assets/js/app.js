8e05db302b70af3ab67bfff5a4f59711ad88ac21
Danny Tran <dannyhaitran@outlook.com>
2015-01-26 02:12:47 -0800

    Second commit-Branching after

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,12 +1,24 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var app =angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'smoothScroll'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+app.controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+    function ($scope, $location, $anchorScroll) {
+        $scope.gotoBottom = function() {
+            // set the location.hash to the id of
+            // the element you wish to scroll to.
+            $location.hash('bottom');
+
+            // call $anchorScroll()
+            $anchorScroll();
+        };
+    }]);
